cc886aa1c5e2821a83a29a9a3fcd08b4cbe41fb9
Enrique Hernandez Castillo <34141975+enriquesid@users.noreply.github.com>
2018-02-26 13:21:39 -0600

    Ready Retail Sale contract (#3)
    * Ready retailsale

---
+++ contracts/RetailSale.sol
@@ -0,0 +1,160 @@
+pragma solidity ^0.4.18;
+
+interface token {
+    function transferFrom(address _from, address _to, uint256 _value) public;
+}
+
+contract CrowdSale {
+    address public beneficiary;
+    uint public startTime;
+    uint public deadline;
+    uint public price;
+    uint public bonus = 0;
+    uint public bonusStart = 0;
+    uint public bonusEnd = 0;
+    uint public milestone = 0;
+    uint public milestoneBonus = 0;
+    bool public milestoneReached = false;
+    token public tokenReward;
+    bool public closed = false;
+
+    event FundTransfer(address backer, uint amount, uint bonus, uint tokens);
+
+    /**
+     * Constrctor function
+     *
+     * Setup the owner
+     */
+    function CrowdSale(
+        address ifSuccessfulSendTo,
+        address addressOfTokenUsedAsReward,
+        uint tokensPerEth,
+        uint startTimeInSeconds,
+        uint endTimeInSeconds
+    ) public {
+        beneficiary = ifSuccessfulSendTo;
+        tokenReward = token(addressOfTokenUsedAsReward);
+        price = tokensPerEth * 1 ether;
+        startTime = startTimeInSeconds;
+        deadline = endTimeInSeconds;
+    }
+
+    /**
+     * Fallback function
+     *
+     * The function without name is the default function that is called whenever anyone sends funds to a contract
+     */
+    function()
+    payable
+    isOpen
+    previousDeadline
+    afterStart
+    public {
+        uint amount = msg.value;
+        uint vp = amount * price;
+        uint b = 0;
+        uint tokens = 0;
+        if (now >= bonusStart && now <= bonusEnd) {
+            b = bonus;
+        }
+        if (this.balance >= milestone && !milestoneReached) {
+            b = milestoneBonus;
+            milestoneReached = true;
+        }
+        if (b == 0) {
+            tokens = vp / 1 ether;
+        } else {
+            tokens = (vp + ((vp * b) / 100)) / 1 ether;
+        }
+        tokenReward.transferFrom(beneficiary, msg.sender, tokens);
+        FundTransfer(msg.sender, msg.value, b, tokens);
+    }
+
+
+
+    modifier afterStart() {
+        require(now >= startTime);
+        _;
+    }
+
+    modifier afterDeadline() {
+        require(now >= deadline);
+        _;
+    }
+
+    modifier previousDeadline() {
+        require(now <= deadline);
+        _;
+    }
+
+    modifier isOwner() {
+        require(msg.sender == beneficiary);
+        _;
+    }
+
+    modifier isClosed() {
+        require(closed);
+        _;
+    }
+
+    modifier isOpen() {
+        require(!closed);
+        _;
+    }
+
+    /**
+     * Close the crowdsale
+     *
+     */
+    function closeCrowdsale()
+    isOwner
+    public {
+        closed = true;
+    }
+
+    /**
+     * Change the bonus percentage
+     * @param _bonus the new bonus percentage.
+     * @param _bonusStart When the bonus starts.
+     * @param _bonusEnd When the bonus ends.
+     */
+    function changeBonus(uint _bonus, uint _bonusStart, uint _bonusEnd)
+    isOwner
+    public {
+        bonus = _bonus;
+        bonusStart = _bonusStart;
+        bonusEnd = _bonusEnd;
+    }
+
+    /**
+     * Change the next milestone
+     * @param _milestone The next milestone amount
+     * @param _milestoneBonus The bonus of the next milestone
+     */
+    function setNextMilestone(uint _milestone, uint _milestoneBonus)
+    isOwner
+    public {
+        milestone = _milestone;
+        milestoneBonus = _milestoneBonus;
+        milestoneReached = false;
+    }
+
+
+    /**
+     * Withdraw the funds
+     *
+     * Checks to see if goal or time limit has been reached, and if so, and the funding goal was reached,
+     * sends the entire amount to the beneficiary. If goal was not reached, each contributor can withdraw
+     * the amount they contributed.
+     */
+    function safeWithdrawal()
+    afterDeadline
+    isClosed
+    isOwner
+    public {
+
+        beneficiary.transfer(this.balance);
+
+    }
+
+}
